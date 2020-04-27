7556daac3a283513e1d59ef0603b1e26d4baa851
Adam Zawadka <zawadka.adam@gmail.com>
2020-04-27 16:40:42 +0200

    reduce duplication

--- app/src/main/java/com/gmail/adam/kidssudoku/MainActivity.java
+++ app/src/main/java/com/gmail/adam/kidssudoku/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private int[][] table = {{1, 0, 3}, {2, 3, 0}, {3, 1, 2}};
     private int[][] result = new int[3][3];
-    private boolean[] answerNo = {false, false, false};
+    private int answerNo = -1;
     private HashMap<String, ImageView> imageViewHashMap = new HashMap<String, ImageView>();
 
     @Override
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         Button checkButton = findViewById(R.id.CheckButton);
-
 
         final ImageView image00 = findViewById(R.id.image00);
         final ImageView image01 = findViewById(R.id.image01);
@@ -61,44 +60,6 @@ public class MainActivity extends AppCompatActivity {
         imageViewHashMap.put("A2", imageA2);
 
 
-        imageA0.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                imageA0.setBackgroundColor(Color.parseColor("#13EF1C"));
-                imageA1.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                imageA2.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                answerNo[0] = true;
-                answerNo[1] = false;
-                answerNo[2] = false;
-
-            }
-        });
-        imageA1.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                imageA0.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                imageA1.setBackgroundColor(Color.parseColor("#13EF1C"));
-                imageA2.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                answerNo[0] = false;
-                answerNo[1] = true;
-                answerNo[2] = false;
-            }
-        });
-        imageA2.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                imageA0.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                imageA1.setBackgroundColor(Color.parseColor("#FAF6F6"));
-                imageA2.setBackgroundColor(Color.parseColor("#13EF1C"));
-                answerNo[0] = false;
-                answerNo[1] = false;
-                answerNo[2] = true;
-            }
-        });
-
         for (final Map.Entry<String, ImageView> stringImageViewEntry : imageViewHashMap.entrySet()) {
             if (!stringImageViewEntry.getKey().contains("A")) {
                 stringImageViewEntry.getValue().setImageResource(getResources().getIdentifier("@drawable/a0" +
@@ -111,15 +72,13 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         if (table[((int) stringImageViewEntry.getKey().charAt(0) - 48)]
                                 [((int) stringImageViewEntry.getKey().charAt(1) - 48)] == 0) {
-                            for (int i = 0; i < answerNo.length; i++) {
-                                if (answerNo[i]) {
-                                    result[((int) stringImageViewEntry.getKey().charAt(0) - 48)]
-                                            [((int) stringImageViewEntry.getKey().charAt(1) - 48)] = i + 1;
-                                    stringImageViewEntry.getValue().setImageResource(
-                                            getResources().getIdentifier("@drawable/a0" + (i + 1)
+
+                            result[((int) stringImageViewEntry.getKey().charAt(0) - 48)]
+                                    [((int) stringImageViewEntry.getKey().charAt(1) - 48)] = answerNo;
+
+                            stringImageViewEntry.getValue().setImageResource(
+                                    getResources().getIdentifier("@drawable/a0" + (answerNo)
                                             , null, MainActivity.this.getPackageName()));
-                                }
-                            }
                         }
 
                     }
@@ -128,6 +87,19 @@ public class MainActivity extends AppCompatActivity {
                 stringImageViewEntry.getValue().setImageResource(getResources().getIdentifier(
                         "@drawable/a0" + ((int) stringImageViewEntry.getKey().charAt(1) - 47),
                         null, this.getPackageName()));
+
+                stringImageViewEntry.getValue().setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        if (answerNo != -1) {
+                            imageViewHashMap.get("A" + (answerNo - 1)).setBackgroundColor(Color.parseColor("#FAF6F6"));
+                        }
+                        stringImageViewEntry.getValue().setBackgroundColor(Color.parseColor("#13EF1C"));
+                        answerNo = (int) stringImageViewEntry.getKey().charAt(1) - 47;
+
+                    }
+                });
             }
         }
 
@@ -139,9 +111,9 @@ public class MainActivity extends AppCompatActivity {
                     for (int j = 0; j < result[i].length; j++) {
                         if (table[i][j] == 0) {
                             if (check(i, j, result[i][j])) {
-                                imageViewHashMap.get(""+ i + j).setBackgroundColor(Color.parseColor("#13EF1C"));
+                                imageViewHashMap.get("" + i + j).setBackgroundColor(Color.parseColor("#13EF1C"));
                             } else {
-                                imageViewHashMap.get(""+ i + j).setBackgroundColor(Color.parseColor("#CA2C2C"));
+                                imageViewHashMap.get("" + i + j).setBackgroundColor(Color.parseColor("#CA2C2C"));
                             }
                         }
                     }
@@ -154,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < result.length; i++) {
             if (result[row][i] == value || result[i][col] == value) {
                 if (!(i == row || i == col))
-                return false;
+                    return false;
             }
         }
         return true;
